5315a43ba7d55e2a51dc214c59aec176ff43dd3e
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-28 06:42:21 +0000

    changes

--- Quizz-App/script.js
+++ Quizz-App/script.js
@@ -6,17 +6,20 @@ async function loadQuestion(){
     const response= await fetch(`${apiUrl}`);
     const data= await response.json();
     // console.log(data.results[0]);
-    showQuestion(data.results[0]);
+    Question(data.results[0]);
 }
 
 
-function showQuestion(data){
+function Question(data){
+    currentCorrectAnswer = data.correct_answer;
     let correctAnswer= data.correct_answer;
     let incorrectAnswer= data.incorrect_answers;
     let optionList= incorrectAnswer;
     optionList.splice(Math.floor(Math.random()*(incorrectAnswer.length+1)), 0, correctAnswer);
     // console.log(optionList);
     // console.log(correctAnswer);
+    // console.log(currentCorrectAnswer);
+    
     _question.innerHTML= `${data.question}`;
     _options.innerHTML = "";
 
@@ -27,5 +30,24 @@ function showQuestion(data){
 loadQuestion();
 
 const questionElement= document.getElementById("question");
-const answerButton= document.getElementById("answer-buttons");
+const answerButton= document.getElementById("answer-btns");
 const nextButton= document.getElementById("nxt-btn");
+
+answerButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn')) {
+        if(e.target.innerText === currentCorrectAnswer){
+            e.target.style.backgroundColor = 'lightgreen';
+        }else{
+            e.target.style.backgroundColor = '#ff9393';
+        }
+        const allBtns = document.querySelectorAll('#answer-btns .btn');
+        allBtns.forEach(btn => {
+            btn.style.pointerEvents = "none";       
+        });
+        const correctBtn = [...allBtns].find(btn => btn.innerText === currentCorrectAnswer);
+        correctBtn.style.backgroundColor = "lightgreen";
+        nextButton.style.display= "block";
+    }
+});
+
+

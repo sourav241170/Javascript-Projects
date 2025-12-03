68c4426031f3a2585804c39c12539f1cdced5049
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-12-03 02:11:46 +0000

    This is a QR Code generator that generates QR code using API

---
+++ QR-code-Generator/script.js
@@ -0,0 +1,16 @@
+let imgBox= document.querySelector("#imgbox")
+let qrImage= document.getElementById("qrimage")
+let qrText= document.getElementById("qrtext")
+
+function generateQr(){
+    if(qrText.value.length> 0){
+        qrImage.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+        }, 1000)
+    }
+    
+}
